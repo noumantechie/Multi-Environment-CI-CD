7c895acc80c3a79519e48898696fc11d77239121
Kanzal Qalandri <kanzalqalandri@gmail.com>
2025-11-24 11:19:47 -0500

    adding tag stuff

---
+++ release.sh
@@ -0,0 +1,16 @@
+# Manual or scripted release process
+VERSION=$1
+
+# Update Chart.yaml
+yq eval ".version = \"$VERSION\"" -i helm/Chart.yaml
+yq eval ".appVersion = \"$VERSION\"" -i helm/Chart.yaml
+
+# Commit the change
+git add helm/Chart.yaml
+git commit -m "chore: bump chart version to $VERSION"
+
+# Tag the commit
+git tag "$VERSION"
+
+# Push both commit and tag
+git push origin "$VERSION"
